1c684e2803b09da1dba1d6148d028c8e9f9cd8ac
SakataRyuichi <ryuichi.design@gmail.com>
2025-04-08 22:41:17 +0900

    Add main page component

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Game from './components/Game';
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <Game />
+    </main>
+  );
+}
